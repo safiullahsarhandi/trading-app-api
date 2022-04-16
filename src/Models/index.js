7abba224dd259b0d1d5d48e4b5f708bfbc7ae7e7
Safeeullah Sarhandi <sarhandi4@gmail.com>
2022-04-16 19:45:31 +0500

    changes

--- src/Models/index.js
+++ src/Models/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+// console.log(process.env.NODE_ENV);
 const config = require(__dirname + '/../Config/database.json')[env];
 const db = {};
 
